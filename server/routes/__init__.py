c47d82f1a8d0f1bd59f742537d05f2e5ff74ae26
Renato Mello <renato.msf@gmail.com>
2019-03-20 13:50:12 +0000

    Fixing previous changes

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -1,8 +1,7 @@
 """Routes"""
 
 from os.path import dirname, basename, isfile
-from flask_restplus import Api
 import glob
-api = Api(app, title='My first Python API', version='1.0', doc='/apidocs/', description='A number-crunching API')
+
 modules = glob.glob(dirname(__file__)+"/*.py")
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
