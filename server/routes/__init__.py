13ea933e30285670082256c55bef7a3a2482a73d
Renato Mello <renato.msf@gmail.com>
2019-03-20 12:35:43 +0000

    Create function to receive integer as input and outputs its double

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -1,7 +1,10 @@
 """Routes"""
 
 from os.path import dirname, basename, isfile
+from flask_restplus import Api
 import glob
+
 modules = glob.glob(dirname(__file__)+"/*.py")
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
 
+api = Api(app, title='My first Python API', version='1.0', doc='/apidocs/', description='A number-crunching API')
